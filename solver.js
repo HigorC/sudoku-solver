8dc97bf8f0ede5e87f39a84a40a9f77a11b09fce
Higor Chaves <higor_oliveira@outlook.com>
2020-06-10 21:12:31 -0400

    Add sudoku easy matrix

---
+++ solver.js
@@ -0,0 +1,13 @@
+const sudoku = [
+    [null, null, 7, null, null, null, null, 3, null],
+    [null, null, null, null, 2, null, 8, null, null],
+    [null, 2, 3, 4, 8, 7, null, 5, null],
+    [null, null, 8, 5, null, null, 1, null, null],
+    [null, 1, 9, null, null, null, 2, 4, null],
+    [null, null, 4, null, null, 3, 6, null, null],
+    [null, 5, null, 2, 3, 6, 7, 9, null],
+    [4, null, 2, null, 5, null, null, null, null],
+    [null, null, null, null, null, null, 5, null, null]
+]
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
