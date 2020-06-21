67e518d6e46ed814033ca6e8c5a6b47a5178416e
Higor Chaves <higor_oliveira@outlook.com>
2020-06-21 09:05:02 -0400

    Files

--- api/app.js
+++ api/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const solver = require('./solver')
 const generator = require('./generator')
